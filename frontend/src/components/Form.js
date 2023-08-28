161656835b8f0564ee3f840f0b6d81600c3faf71
Rodrigo Viana <roviol@gmail.com>
2023-08-28 12:54:14 -0300

    feature: reading data from db

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -17,14 +17,49 @@ const InputArea = styled.div`
   flex-direction: column;
 `;
 
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`;
+
+const Label = styled.label``;
+
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #2c73d2;
+  color: white;
+  height: 42px;
+`;
+
 const Form = ({ onEdit }) => {
   const ref = useRef();
 
   return (
     <FormContainer ref={ref}>
       <InputArea>
-        CONTINUAR DE 19:50 - https://www.youtube.com/watch?v=voXTVTW73E8
+        <Label>Nome</Label>
+        <Input name="nome" />
       </InputArea>
+      <InputArea>
+        <Label>E-mail</Label>
+        <Input name="email" type="email" />
+      </InputArea>
+      <InputArea>
+        <Label>Telefone</Label>
+        <Input name="fone" />
+      </InputArea>
+      <InputArea>
+        <Label>Data de Nascimento</Label>
+        <Input name="data_nascimento" type="date" />
+      </InputArea>
+
+      <Button type="submit">SALVAR</Button>
     </FormContainer>
   );
 };
